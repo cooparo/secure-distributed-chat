cd439ca0f75febf62dcd5852f4b0a0f51f8b3ad6
0siriz <0siriz@proton.me>
2025-11-21 12:58:36 +0100

    Added database package with sqlc (#18)
    * Added sqlc to generate "ORM"
    
    * Added method to get the id of an identity
    
    * Added sessions table
    
    * Added messages table
    
    * Minor rename of migration file
    
    * Missed sqlc generate
    
    * Sessions queries and schema update
    
    * Added update query to identity
    
    * Renamed timestamp to time on message table
    
    * Added update queries to session
    
    * Added message queries
    
    * Added queries to get random identities

---
+++ db/queries/identity.sql
@@ -0,0 +1,35 @@
+-- name: AddIdentity :exec
+INSERT INTO identity (
+	address, key_bundle, net_addr_bundle_time, net_addr_bundle
+) VALUES (
+	?, ?, ?, ?
+);
+
+-- name: UpdateIdentity :exec
+UPDATE identity
+SET net_addr_bundle_time = ?, net_addr_bundle = ?
+WHERE address = ?;
+
+-- name: DeleteIdentity :exec
+DELETE FROM identity
+WHERE address = ?;
+
+-- name: GetIdentity :one
+SELECT key_bundle, net_addr_bundle_time, net_addr_bundle
+FROM identity
+WHERE address = ?;
+
+-- name: GetIdentityId :one
+SELECT id
+FROM identity
+WHERE address = ?;
+
+-- name: GetRandomIdentity :one
+SELECT address, key_bundle, net_addr_bundle_time, net_addr_bundle
+FROM identity
+ORDER BY RANDOM() LIMIT 1;
+
+-- name: GetRandomIdentities :many
+SELECT address, key_bundle, net_addr_bundle_time, net_addr_bundle
+FROM identity
+ORDER BY RANDOM() LIMIT ?;

acbb1bf3aa8fb3749b6b3eab9683c39a24a6bdb6
0siriz <0siriz@proton.me>
2025-11-24 10:09:21 +0100

    Database and migration overhaul (#22)
    * golang-migrate instead of dbmate for migration
    
    * Some major moving around of the database
    
    * Database migration and library made

--- cmd/gratcli/main.go
+++ cmd/gratcli/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/cooparo/secure-distributed-chat/cmd/gratcli/command"
+import (
+	"io"
+	"os"
+
+	"github.com/cooparo/secure-distributed-chat/pkg/logger"
+	"github.com/spf13/cobra"
+)
+
+var (
+	verbose bool
+	address string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gratcli",
+	Short: "Chat",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logger.Init(verbose)
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil // TODO: make it communicate with IPC
+	},
+}
 
 func main() {
-	command.Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		io.WriteString(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
+	rootCmd.Flags().StringVarP(&address, "address", "a", "127.0.0.1:1337", "Address to connect to")
 }

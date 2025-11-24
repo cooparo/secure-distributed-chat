a5e9198c60e27e09e3a286b6b280d98bc078031f
0siriz <0siriz@proton.me>
2025-11-24 10:18:02 +0100

    Added session management (#23)
    * Added Session and SessionManager
    
    * Removed some comments

---
+++ pkg/session/session.go
@@ -0,0 +1,11 @@
+package session
+
+import (
+	"github.com/cooparo/secure-distributed-chat/pkg/doubleratchet"
+	"github.com/cooparo/secure-distributed-chat/pkg/identity"
+)
+
+type Session struct {
+	Address identity.IdentityAddress
+	Ratchet doubleratchet.DoubleRatchet
+}

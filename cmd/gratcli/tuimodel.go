1eb8301b70d0dc1464018fad89b182601da30c58
cooparo <github.ambitious226@slmails.com>
2026-03-15 17:44:40 +0100

    fix(tui): increase message fetch frequency to every second

--- cmd/gratcli/tuimodel.go
+++ cmd/gratcli/tuimodel.go
@@ -172,7 +172,7 @@ func (m model) Init() tea.Cmd {
 }
 
 func tickCmd() tea.Cmd {
-	return tea.Tick(3*time.Second, func(t time.Time) tea.Msg {
+	return tea.Tick(1*time.Second, func(t time.Time) tea.Msg {
 		return tickMsg(t)
 	})
 }
